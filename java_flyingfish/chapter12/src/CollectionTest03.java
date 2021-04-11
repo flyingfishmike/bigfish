df14e9128ff5a19ba13939bd6171eaf870709302
flyingfishmike <czh12696@163.com>
2021-04-11 16:34:42 +0800

    CollectionTest03

--- java_flyingfish/chapter12/src/CollectionTest03.java
+++ java_flyingfish/chapter12/src/CollectionTest03.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 public class CollectionTest03 {
     public static void main(String[] args) {
@@ -21,5 +22,19 @@ class User{
 
     public User(String name) {
         this.name = name;
+    }
+    // 重写equals方法
+    // 只要姓名相同就表示同一个用户
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
